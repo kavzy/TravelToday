38f7a8628b604fdcc0e0369fef5d9b855678085f
kavzy <kaveesha.s.ekanayake@gmail.com>
2022-06-26 11:57:04 +0530

    dashbord done

---
+++ Admin/footer.php
@@ -0,0 +1,10 @@
+<footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2022 &copy; TravelToday</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class='text-danger'><i data-feather="heart"></i></span> by <a href="#">Travel Today</a></p>
+                    </div>
+                </div>
+            </footer>
---
+++ Admin/side_navbar.php
@@ -0,0 +1,56 @@
+<div class="sidebar-menu">
+        <ul class="menu">
+            
+                
+                <li class='sidebar-title'>Main Menu</li>
+                
+            
+                
+                <li class="sidebar-item  ">
+
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="briefcase" width="20"></i> 
+                        <span>Manage Bookings</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="inbox" width="20"></i> 
+                        <span>Messages</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="users" width="20"></i> 
+                        <span>Manage Customers</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="file-text" width="20"></i> 
+                        <span>Manage Payments</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="file-text" width="20"></i> 
+                        <span>Manage Expenses</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="user" width="20"></i> 
+                        <span>Manage Admins</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="grid" width="20"></i> 
+                        <span>Manage Packages</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="file-plus" width="20"></i> 
+                        <span>Generate Report</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="settings" width="20"></i> 
+                        <span>Account Settings</span>
+                    </a>
+                    <a href="index.html" class='sidebar-link'>
+                        <i data-feather="log-out" width="20"></i> 
+                        <span>Logout</span>
+                    </a>
+
+                    
+                </li>
+                        
+        </ul>
+    </div>

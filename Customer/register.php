c683f1ec571e5457b2e56aaa39afca342a9d3495
kavzy <kaveesha.s.ekanayake@gmail.com>
2022-06-26 11:18:01 +0530

    init commit

---
+++ Customer/register.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Today - Register</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    
+    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+
+<body>
+    <div id="auth">
+        
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 col-sm-12 mx-auto">
+            <div class="card pt-4">
+                <div class="card-body">
+                    <div class="text-center mb-5">
+                        <img src="../img/logo.png" height="48" class='mb-4'>
+                        <h3>Customer Registration</h3>
+                        <p>Please fill the form to register.</p>
+                        <a href="../index.html"><i data-feather="arrow-left" width="20"></i> Back to home</a>
+                    </div>
+                    <form action="index.html">
+                        <div class="row">
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                    <label for="first-name-column">First Name</label>
+                                    <input type="text" id="first-name-column" class="form-control"  name="fname-column">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                    <label for="last-name-column">Last Name</label>
+                                    <input type="text" id="last-name-column" class="form-control"  name="lname-column">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                    <label for="username-column">Username</label>
+                                    <input type="text" id="username-column" class="form-control" name="username-column">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                    <label for="country-floating">Country</label>
+                                    <input type="text" id="country-floating" class="form-control" name="country-floating">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" id="password" class="form-control" name="password">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                    <label for="email-id-column">Email</label>
+                                    <input type="email" id="email-id-column" class="form-control" name="email-id-column">
+                                </div>
+                            </div>
+                        </diV>
+
+                                <a href="login.php">Have an account? Login</a>
+                        <div class="clearfix">
+                            <button class="btn btn-primary float-end">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+    <script src="assets/js/feather-icons/feather.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,81 @@
+<?php include './Authenticate.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Today - Login</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+
+<body>
+    <div id="auth">
+        
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 col-sm-12 mx-auto">
+            <div class="card pt-4">
+                <div class="card-body">
+                    <div class="text-center mb-5">
+                        <img src="img/logo.png" height="48" class='mb-4'>
+                        <h3>Login</h3>
+                        <p>Please sign in to continue to Travel Today.</p>
+                        <a href="index.html"><i data-feather="arrow-left" width="20"></i> Back to home</a>
+                    </div>
+                    <?php $auth = new Authenticate(); ?>
+                    <?php if(isset($_POST['username']) || isset($_POST['password'])): ?>
+                    <?php $auth->login($_POST['username'], $_POST['password']); ?>
+                    <?php endif; ?>
+                    <form action="" method="post">
+                        <div class="form-group position-relative has-icon-left">
+                            <label for="username">Username</label>
+                            <div class="position-relative">
+                                <input type="text" class="form-control" name="username" id="username">
+                                <div class="form-control-icon">
+                                    <i data-feather="user"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left">
+                            <div class="clearfix">
+                                <label for="password">Password</label>
+                                <a href="auth-forgot-password.html" class='float-end'>
+                                    <small>Forgot password?</small>
+                                </a>
+                            </div>
+                            <div class="position-relative">
+                                <input type="password" name="password" class="form-control" id="password">
+                                <div class="form-control-icon">
+                                    <i data-feather="lock"></i>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='form-check clearfix my-4'>
+                            <div class="checkbox float-start">
+                                <input type="checkbox" id="checkbox1" class='form-check-input' >
+                                <label for="checkbox1">Remember me</label>
+                            </div>
+                        </div>
+                        <div class="clearfix">
+                            <button class="btn btn-primary float-end">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+    <script src="assets/js/feather-icons/feather.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
